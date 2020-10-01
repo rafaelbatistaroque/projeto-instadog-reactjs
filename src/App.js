9a817276c38231d038ad3f899730346becfce503
Rafael Batista <maranata_rafa@hotmail.com>
2020-09-30 23:31:59 -0400

    Foi criado rotas e navegações principais.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "./Global/Header/Header";
 import Footer from "./Global/Footer/Footer";
+import Rotas from "./Global/Rotas/Rotas";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Rotas />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
